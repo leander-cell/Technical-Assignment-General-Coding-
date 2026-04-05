8af4a26a44be82e5f3fccec1b685f8f337fad081
leander-cell <leander@browserstack.com>
2026-04-05 23:53:31 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,1 +1,14 @@
+n = int(input("Enter your number:"))
 
+if n<=1:
+    print(n, "is not a prime number")
+
+else:
+    for i in range(2, n +1):
+       if n % i == 0:
+           break
+    if n == i:
+        print(n, "is a prime number")
+    
+    else:
+        print(n, "is not a prime number")
